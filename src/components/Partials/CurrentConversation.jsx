b638a1ba35bad724d318f2d59a8938df1b2152c5
Ember Doyle <doyle.ember@gmail.com>
2024-11-25 12:06:24 -0700

    Add list of messages when conversation selected

--- src/components/Partials/CurrentConversation.jsx
+++ src/components/Partials/CurrentConversation.jsx
@@ -4,7 +4,18 @@ function NavBar({seletedConversation}) {
     if(seletedConversation == null) return;
     console.log(seletedConversation)
 
-    return 
+    return (
+        <ul>
+                    {seletedConversation.map((message) => {
+                        return(
+                        <li key={message.id}>
+                            <div className="name" >{message.author.first_name} {message.author.last_name}</div>
+                            <div className="time">{message.createdAt}</div>
+                            <div className="text">{message.text}</div>
+                        </li>)
+                    })}
+                </ul>
+    )
 
     
 }
